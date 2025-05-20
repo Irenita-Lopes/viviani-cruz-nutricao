c362559ee0c0c71ca03991f0439ff106ecfe0c5e
Irenita-Lopes <irenita.f.lopes@gmail.com>
2025-05-20 14:50:01 -0300

    adicionando algumas telas

--- src/app/screens/About/index.tsx
+++ src/app/screens/About/index.tsx
@@ -1,7 +1,17 @@
 const About = () => {
     return (
         <div className="h-screen" id="about">
-            Aqui é about
+
+            <h1>
+                Viviani Aparecida Cruz
+            </h1>
+            <h2>
+                Técnica em Nutrição e Dietética e Fundadora da AlimentAfros
+            </h2>
+            <p>
+                Sou apaixonada por alimentar corpos, histórias e afetos. Com formação técnica em Nutrição e Dietética e experiência em Unidades de Alimentação e Nutrição, hoje atuo com foco na nutrição da população negra, promovendo saúde com acolhimento, identidade e ancestralidade.
+                Acredito que comer bem é um ato político e de autocuidado, e minha missão é tornar o cuidado nutricional mais acessível, respeitoso e potente para pessoas pretas.
+            </p>
         </div>
     );
 }
